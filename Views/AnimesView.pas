4648d1824102d37020272805e4e4c3d0c50cfe1e
Dreamless2 <tiagonet50@gmail.com>
2026-08-16 20:59:11 -0300

    Cine.dpr Cine.dproj CineView.dfm FilmesView.dfm FilmesView.pas

---
+++ Views/AnimesView.pas
@@ -0,0 +1,24 @@
+unit AnimesView;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TAnimesMain = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  AnimesMain: TAnimesMain;
+
+implementation
+
+{$R *.dfm}
+
+end.
