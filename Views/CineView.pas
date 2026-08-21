4a145f7a7dc5f942a2a3475af8d0e1e87a77fe4a
Dreamless2 <tiagonet50@gmail.com>
2026-08-21 10:37:34 -0300

    CineView.pas

--- Views/CineView.pas
+++ Views/CineView.pas
@@ -3,8 +3,8 @@ unit CineView;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Winapi.Windows, Winapi.Messages, System.Classes,
+  Vcl.Controls, Vcl.Forms, Vcl.StdCtrls, Vcl.ExtCtrls,
   FilmesView, SeriesView, AnimesView;
 
 type
