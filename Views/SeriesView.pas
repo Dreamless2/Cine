9c2e4361104a6d9dbed314ebf4d40079e2508887
Dreamless2 <tiagonet50@gmail.com>
2026-08-28 14:08:44 -0300

    SeriesView.pas

--- Views/SeriesView.pas
+++ Views/SeriesView.pas
@@ -154,19 +154,14 @@ begin
     EstreiaBox.Text := AMedia.DataEstreia;
     AlternativoBox.Text := AMedia.NomeAlternativo;
     SerieBox.Text := GerarTagFilme(AMedia.Nome);
+    LocalBox.Text := AMedia.LocalProducao;
+    IdiomaBox.Text := AMedia.IdiomaOriginal;
+    ShowrunnersBox.Text := AMedia.Showrunners;
     GeneroBox.Text := AMedia.Generos;
     TagsBox.Text := AMedia.Tags;
     DiretorBox.Text := AMedia.Diretores;
     ArtistasBox.Text := AMedia.Artistas;
     ProdutoraBox.Text := AMedia.Produtoras;
-    LocalBox.Text := AMedia.LocalProducao;
-    IdiomaBox.Text := AMedia.IdiomaOriginal;
-    ReferenciaBox
-    AutoresBox.Box
-    FranquiaBox.Text
-    ShowrunnersBox.Text
-
-
   finally
     FMidiaEvents.ReativarEventos;
   end;
