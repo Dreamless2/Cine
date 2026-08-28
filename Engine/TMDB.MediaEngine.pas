61310543599b4acece04e2e45595caaf16fa4413
Dreamless2 <tiagonet50@gmail.com>
2026-08-28 18:33:30 -0300

    TMDB.MediaEngine.pas

--- Engine/TMDB.MediaEngine.pas
+++ Engine/TMDB.MediaEngine.pas
@@ -217,7 +217,7 @@ begin
     begin
       LArr := LJson.GetValue<TJSONArray>('production_countries', nil);
       if Assigned(LArr) and (LArr.Count > 0) then
-        Result.LocalProducao := MapearPais(FormatarParaTag((LArr.Items[0] as TJSONObject).GetValue<string>('name', '')))
+        Result.LocalProducao := FormatarParaTag(m(LArr.Items[0] as TJSONObject).GetValue<string>('name', '')))
       else
       begin
         LOriginArr := LJson.GetValue<TJSONArray>('origin_country', nil);
