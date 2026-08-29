402e1dea362af3de7cd26e934e2febc1583b9b5c
Dreamless2 <tiagonet50@gmail.com>
2026-08-29 15:42:57 -0300

    FilmesView.pas

--- Views/FilmesView.pas
+++ Views/FilmesView.pas
@@ -170,6 +170,7 @@ begin
     EstreiaBox.Text := ValorOuPadrao(AMedia.DataEstreia);
     AlternativoBox.Text := ValorOuPadrao(AMedia.NomeAlternativo);
     FilmeBox.Text := GerarTag(ValorOuPadrao(AMedia.Nome));
+    FranquiaBox.Text := ValorOuPadrao(A);
     GeneroBox.Text := ValorOuPadrao(AMedia.Generos);
     TagsBox.Text := ValorOuPadrao(AMedia.Tags);
     DiretorBox.Text := ValorOuPadrao(AMedia.Diretores);
