4482581a953034aa3b3fc1fbf8bf359abdc130f2
Dreamless2 <tiagonet50@gmail.com>
2026-08-29 19:09:49 -0300

    SeriesView.pas

--- Views/SeriesView.pas
+++ Views/SeriesView.pas
@@ -180,6 +180,11 @@ begin
     AlternativoBox.Text := ValorOuPadrao(AMedia.NomeAlternativo);
     SerieBox.Text := GerarTag(ValorOuPadrao(AMedia.Nome));
     FranquiaBox.Text := ValorOuPadrao(AMedia.Franquia);
+    LocalBox.Text;
+    IdiomaBox.Text;
+    ReferenciaBox.Text;
+    AutoresBox.Text;
+    Showrunners
     GeneroBox.Text := ValorOuPadrao(AMedia.Generos);
     TagsBox.Text := ValorOuPadrao(AMedia.Tags);
     DiretorBox.Text := ValorOuPadrao(AMedia.Diretores);
