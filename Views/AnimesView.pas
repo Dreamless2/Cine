105c6ffeb58885632c1bee796413be6eeae30f0d
Dreamless2 <tiagonet50@gmail.com>
2026-08-28 22:57:33 -0300

    AnimesView.dfm AnimesView.pas

--- Views/AnimesView.pas
+++ Views/AnimesView.pas
@@ -4,7 +4,9 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Mask, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Mask, Vcl.ExtCtrls,
+  TMDB.ApiClient, System.JSON, TMDB.MediaEngine, System.Threading, ResumoBuilder,
+  System.UITypes, TMDB.KeyStore, MidiaFormEvents;
 
 type
   TAnimesMain = class(TForm)
@@ -74,5 +76,165 @@ var
 implementation
 
 {$R *.dfm}
+
+constructor TAnimesMain.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+
+  if HasStoredApiKey then
+    FTMDBClient := TTMDBClient.Create(LoadApiKey)
+  else
+    FTMDBClient := nil;
+
+  FMidiaEvents := TMidiaFormHelper.Create(
+    [AudioBox, SinopseBox, OriginalBox, EstreiaBox, AlternativoBox, SerieBox,
+    MCUBox, LocalBox, IdiomaBox, ReferenciaBox, AutoresBox, FranquiaBox, ShowrunnersBox,
+    GeneroBox, TagsBox, DiretorBox, ArtistasBox, ProdutoraBox],
+    NomeBox, SerieBox, ResumoBox,
+    function: string
+    begin
+      Result := MontarResumo(
+        NomeBox.Text, AudioBox.Text, SinopseBox.Text, OriginalBox.Text, EstreiaBox.Text, AlternativoBox.Text,
+        TagsBox.Text, SerieBox.Text, MCUBox.Text, LocalBox.Text, IdiomaBox.Text, ReferenciaBox.Text, AutoresBox.Text,
+        FranquiaBox.Text, ShowrunnersBox.Text, GeneroBox.Text, DiretorBox.Text, ArtistasBox.Text, ProdutoraBox.Text
+      );
+    end,
+    function(ANome: string): string
+    begin
+      Result := GerarTag(ANome);
+    end);
+  FMidiaEvents.AtualizarResumo;
+  CodigoBox.OnKeyPress := Buscar;
+  CodigoBox.OnExit := BuscarExit;
+  CopiarButton.OnClick := CopiarButton_Click;
+  LimparPainel(PanelDesktop);
+end;
+
+function ExceptionDetalhes(E: Exception): string;
+begin
+  Result := E.ClassName + ': ' + E.Message;
+
+  while Assigned(E.InnerException) do
+  begin
+    E := E.InnerException;
+
+    Result := Result + sLineBreak + '  -> ' + E.ClassName + ': ' + E.Message;
+  end;
+end;
+
+procedure TAnimesMain.FormDestroy(Sender: TObject);
+begin
+  FMidiaEvents.Free;
+  FTMDBClient.Free;
+end;
+
+procedure TAnimesMain.LimparPainel(Painel: TPanel);
+var
+  i: Integer;
+begin
+  for i := 0 to Painel.ControlCount - 1 do
+  begin
+    if Painel.Controls[i] is TEdit then
+      TEdit(Painel.Controls[i]).Text := '--';
+
+    if Painel.Controls[i] is TMaskEdit then
+      TMaskEdit(Painel.Controls[i]).Text := '--';
+  end;
+end;
+
+procedure TAnimesMain.PreencherComMedia(const AMedia: TMediaData);
+begin
+  FMidiaEvents.DesativarEventos;
+  try
+    NomeBox.Text := AMedia.Nome;
+    SinopseBox.Text := AMedia.Sinopse;
+    OriginalBox.Text := AMedia.NomeOriginal;
+    EstreiaBox.Text := AMedia.DataEstreia;
+    AlternativoBox.Text := AMedia.NomeAlternativo;
+    AnimeBox.Text := GerarTag(AMedia.Nome);
+    LocalBox.Text := AMedia.LocalProducao;
+    IdiomaBox.Text := GerarTag(AMedia.IdiomaOriginal);
+    ShowrunnersBox.Text := AMedia.Showrunners;
+    GeneroBox.Text := AMedia.Generos;
+    TagsBox.Text := AMedia.Tags;
+    DiretorBox.Text := AMedia.Diretores;
+    ArtistasBox.Text := AMedia.Artistas;
+    ProdutoraBox.Text := AMedia.Produtoras;
+  finally
+    FMidiaEvents.ReativarEventos;
+  end;
+  FMidiaEvents.AtualizarResumo;
+end;
+
+procedure TAnimesMain.Buscar(Sender: TObject; var Key: Char);
+var
+  LMovieId: Integer;
+  LFuture: IFuture<TJSONObject>;
+  LJson: TJSONObject;
+  LMedia: TMediaData;
+begin
+  if Key = #13 then
+  begin
+    Key := #0;
+
+    if not TryStrToInt(CodigoBox.Text, LMovieId) then
+    begin
+      MessageDlg('Digite o código do filme no TMDB.', mtWarning, [mbOK], 0);
+      CodigoBox.SetFocus;
+      Exit;
+    end;
+
+    if not Assigned(FTMDBClient) then
+    begin
+    MessageDlg('A chave da API do TMDB não está configurada.', mtWarning, [mbOK], 0);
+      Exit;
+    end;
+
+    Screen.Cursor := crHourGlass;
+    try
+      try
+        LFuture := FTMDBClient.GetTvShowAsync(LMovieId);
+        LJson := LFuture.Value;
+        try
+          LMedia := ProcessarMidiaTMDB(LJson.ToJSON, True);
+          PreencherComMedia(LMedia);
+        finally
+          LJson.Free;
+        end;
+      except
+        on E: EAggregateException do
+        begin
+         if E.Count > 0 then
+            begin
+              var Erros: string := '';
+              for var I := 0 to E.Count - 1 do
+              begin
+                Erros := Erros + '• ' + E.InnerExceptions[I].Message + sLineBreak;
+              end;
+              MessageDlg('Erros TMDB encontrados:' + sLineBreak + Erros, mtError, [mbOK], 0);
+          end
+          else
+            MessageDlg('Erro TMDB:' + sLineBreak + E.Message, mtError, [mbOK], 0);
+         end;
+      end;
+    finally
+      Screen.Cursor := crDefault;
+    end;
+  end;
+end;
+
+procedure TAnimesMain.BuscarExit(Sender: TObject);
+var
+  LKey: Char;
+begin
+  LKey := #13;
+  Buscar(Sender, LKey);
+end;
+
+procedure TAnimesMain.CopiarButton_Click(Sender: TObject);
+begin
+  ResumoBox.SelectAll;
+  ResumoBox.CopyToClipboard;
+end;
 
 end.
