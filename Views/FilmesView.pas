700621363414f30cdad8b78de490a467fc68a58f
Dreamless2 <tiagonet50@gmail.com>
2026-08-29 14:07:11 -0300

    FilmesView.pas

--- Views/FilmesView.pas
+++ Views/FilmesView.pas
@@ -286,7 +286,7 @@ procedure TFilmesMain.CopiarButton_Click(Sender: TObject);
 begin
   ResumoBox.SelectAll;
   ResumoBox.CopyToClipboard;
-  Application.MessageBox('Copiado com sucesso.'), 'Cine - Filmes', MB_YESNO + MB_ICONINFORMATION);
+  Application.MessageBox('Copiado com sucesso.', 'Cine - Filmes', MB_YESNO + MB_ICONINFORMATION);
 end;
 
 procedure TFilmesMain.SalvarButton_Click(Sender: TObject);
