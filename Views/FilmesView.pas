a00fcf6245a6de3ae2a4c3e516f7bedf5f35191f
Dreamless2 <tiagonet50@gmail.com>
2026-08-28 23:42:32 -0300

    FilmesView.pas

--- Views/FilmesView.pas
+++ Views/FilmesView.pas
@@ -197,29 +197,26 @@ begin
           LJson.Free;
         end;
       except
-  on E: EAggregateException do
-  begin
-    if E.Count > 0 then
-    begin
-      var MsgErro := E.InnerExceptions[0].Message; // Pega o primeiro erro da lista
-
-      // Se for o erro do TMDB, extrai apenas o texto relevante
-      if MsgErro.Contains('"status_message":') then
-      begin
-        var PosInicio := MsgErro.IndexOf('"status_message":') + 18;
-        MsgErro := MsgErro.Substring(PosInicio).Replace('"', '').Replace('}', '').Trim;
-      end
-      else if MsgErro.Contains('404') then
-      begin
-        MsgErro := 'O recurso solicitado não foi encontrado.';
+        on E: EAggregateException do
+        begin
+          if E.Count > 0 then
+          begin
+            var MsgErro := E.InnerExceptions[0].Message;
+            if MsgErro.Contains('"status_message":') then
+            begin
+              var PosInicio := MsgErro.IndexOf('"status_message":') + 18;
+              MsgErro := MsgErro.Substring(PosInicio).Replace('"', '').Replace('}', '').Trim;
+            end
+            else if MsgErro.Contains('404') then
+            begin
+              MsgErro := 'O recurso solicitado não foi encontrado.';
+            end;
+            MessageDlg('Erro TMDB: ' + MsgErro, mtError, [mbOK], 0);
+          end
+          else
+          MessageDlg('Erro TMDB: ' + E.Message, mtError, [mbOK], 0);
+        end;
       end;
-
-      MessageDlg('Erro TMDB: ' + MsgErro, mtError, [mbOK], 0);
-    end
-    else
-      MessageDlg('Erro TMDB: ' + E.Message, mtError, [mbOK], 0);
-  end;
-end;
     finally
       Screen.Cursor := crDefault;
     end;
