0bf958e00bb015072228c06b543662a396237ca0
Dreamless2 <tiagonet50@gmail.com>
2026-08-29 11:34:20 -0300

    CineContext.pas

--- Data/CineContext.pas
+++ Data/CineContext.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
   FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf,
   FireDAC.DApt.Intf, FireDAC.Stan.StorageJSON, Data.DB, FireDAC.Comp.DataSet,
-  FireDAC.Comp.Client, System.IOUtils;
+  FireDAC.Comp.Client, System.IOUtils, Vcl.Dialogs;
 
 type
   THistoricoDataModule = class(TDataModule)
