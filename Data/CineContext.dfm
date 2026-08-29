4c3f9c5ece7cc978fd7699e515d17d2beca2c9d0
Dreamless2 <tiagonet50@gmail.com>
2026-08-29 10:20:02 -0300

    Cine.dpr Cine.dproj CineContext.dfm CineContext.pas

--- Data/CineContext.dfm
+++ Data/CineContext.dfm
@@ -1,4 +1,19 @@
 object DataModule1: TDataModule1
   Height = 480
   Width = 640
+  object HistoricoTable: TFDMemTable
+    FetchOptions.AssignedValues = [evMode]
+    FetchOptions.Mode = fmAll
+    ResourceOptions.AssignedValues = [rvSilentMode]
+    ResourceOptions.SilentMode = True
+    UpdateOptions.AssignedValues = [uvCheckRequired, uvAutoCommitUpdates]
+    UpdateOptions.CheckRequired = False
+    UpdateOptions.AutoCommitUpdates = True
+    Left = 152
+    Top = 80
+  end
+  object FDStanStorageJSONLink1: TFDStanStorageJSONLink
+    Left = 368
+    Top = 304
+  end
 end
--- Data/CineContext.pas
+++ Data/CineContext.pas
@@ -3,12 +3,19 @@ unit CineContext;
 interface
 
 uses
-  System.SysUtils, System.Classes;
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf,
+  FireDAC.DApt.Intf, FireDAC.Stan.StorageJSON, Data.DB, FireDAC.Comp.DataSet,
+  FireDAC.Comp.Client;
 
 type
   TDataModule1 = class(TDataModule)
+    HistoricoTable: TFDMemTable;
+    FDStanStorageJSONLink1: TFDStanStorageJSONLink;
   private
     { Private declarations }
+    procedure DataModuleCreate(Sender: TObject);
+    procedure DataModuleDestroy(Sender: TObject);
   public
     { Public declarations }
   end;
