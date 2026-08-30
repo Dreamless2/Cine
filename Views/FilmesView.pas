d03d54b3259bc2352b382772e6625530ae9fb2b8
Dreamless2 <tiagonet50@gmail.com>
2026-08-30 18:02:43 -0300

    ResumoBuilder.pas

--- Views/FilmesView.pas
+++ Views/FilmesView.pas
@@ -151,7 +151,7 @@ begin
       TEdit(Painel.Controls[i]).Text := '--';
 
     if Painel.Controls[i] is TMaskEdit then
-      TMaskEdit(Painel.Controls[i]).Text := '--';
+      TMaskEdit(Painel.Controls[i]).Text := '';
   end;
 end;
 
