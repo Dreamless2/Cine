55bece2e5dfe667f080ddf6d0d54c9e605593646
Dreamless2 <tiagonet50@gmail.com>
2026-08-30 14:59:15 -0300

    ResumoBuilder.pas

--- Builders/ResumoBuilder.pas
+++ Builders/ResumoBuilder.pas
@@ -23,8 +23,16 @@ function MontarResumo(const ANomeBox, AAudioBox, ASinopseBox, AOriginalBox, AEst
 implementation
 
 function ValorOuTraco(const AValor: string): string;
+var
+  LValor: string;
 begin
-  if AValor.Trim.IsEmpty then Result := '--' else Result := AValor;
+  LValor := AValor.Trim;
+
+  if LValor.IsEmpty or
+     (LValor = '__/__/____') then
+    Result := '--'
+  else
+    Result := LValor;
 end;
 
 function LimparMascaraData(const AValor: string): string;
